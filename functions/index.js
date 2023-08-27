e84d8aac1c5bbc60fff551b14b8ba45687ce6821
Rajibul Islam <rajibulski110@gmail.com>
2023-08-27 19:35:46 +0530

    /afk command done!

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 // Firebase logic encapsulated function
-require(`./firebase/config`);
-const dbWrite = require("./firebase/dbWrite");
+// write
+const { documentWrite, serverTimestamp } = require("./firebase/dbWrite");
+const { documentAdd, documentUpdate, documentDelete, error } =
+  documentWrite("users");
 
-const { documentAdd, documentUpdate, error, serverTimestamp } =
-  dbWrite("users");
+// read
+const documentRead = require("./firebase/dbRead");
 
 // Telegraf essential initlization
 const botToken = "6076138177:AAEJBj7-sHaVQg_KcXIp_64YA1pRjHtzBtE";
@@ -16,7 +18,9 @@ const {
   getUserID,
   getUserName,
   getReplyOptions,
-  fullNameWithTag
+  fullNameWithTag,
+  getCommandArguments,
+  getReason
 } = require("./encapsulation/telegraf-logic");
 
 // Reusable message strings
@@ -27,17 +31,42 @@ bot.start((ctx) => {
   ctx.reply(helloMessage, getReplyOptions(ctx.message));
 });
 
-bot.command("afk", (ctx) => {
-  const userID = getUserID(ctx);
+bot.command("afk", async (ctx) => {
+  const userID = `${getUserID(ctx)}`;
   const nameTag = fullNameWithTag(ctx);
+  const reasonRes = getReason(ctx);
+  const reason = reasonRes ? reasonRes : null;
+
+  // check if document with same id exists
+  const { document } = await documentRead("users", userID);
+
+  if (document) {
+    documentUpdate(userID, { afkAt: serverTimestamp(), reason });
+    reason
+      ? ctx.reply(
+          `${nameTag} is now afk.\nUpdated afk <em>time</em> and <em>reason</em>!\n<strong>Reason:</strong> ${reason}`,
+          getReplyOptions(ctx.message)
+        )
+      : ctx.reply(
+          `${nameTag} is now afk.\nUpdated afk <em>time</em>!`,
+          getReplyOptions(ctx.message)
+        );
+    return;
+  }
 
   const data = {
-    afkAt: serverTimestamp()
+    afkAt: serverTimestamp(),
+    reason
   };
 
   documentAdd(userID, data);
 
-  ctx.reply(`${nameTag} is now afk!`, getReplyOptions(ctx.message));
+  reason
+    ? ctx.reply(
+        `${nameTag} is now afk!\n<strong>Reason:</strong> ${reason}`,
+        getReplyOptions(ctx.message)
+      )
+    : ctx.reply(`${nameTag} is now afk!`, getReplyOptions(ctx.message));
 });
 
 // deployment
