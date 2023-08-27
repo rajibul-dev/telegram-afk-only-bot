d5c06a5347ebcc3cb678eb709bec2d135a1470fb
Rajibul Islam <rajibulski110@gmail.com>
2023-08-27 23:53:25 +0530

    all the situation handled except the @username one

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,6 @@
+// import time formatter tool date-fns
+const { formatDistanceToNowStrict } = require("date-fns");
+
 // Firebase logic encapsulated function
 // write
 const { documentWrite, serverTimestamp } = require("./firebase/dbWrite");
@@ -20,17 +23,23 @@ const {
   getReplyOptions,
   fullNameWithTag,
   getCommandArguments,
-  getReason
+  getReason,
+  getTaggedPersonData
 } = require("./encapsulation/telegraf-logic");
 
 // Reusable message strings
 const helloMessage = `Hello there! This is an afk bot, use /afk to see what it does.`;
 
 // commands
-bot.start((ctx) => {
-  ctx.reply(helloMessage, getReplyOptions(ctx.message));
+const startCommands = ["start", "help"];
+
+startCommands.forEach((command) => {
+  bot.command(command, (ctx) => {
+    ctx.reply(helloMessage, getReplyOptions(ctx.message));
+  });
 });
 
+// /afk
 bot.command("afk", async (ctx) => {
   const userID = `${getUserID(ctx)}`;
   const nameTag = fullNameWithTag(ctx);
@@ -40,7 +49,7 @@ bot.command("afk", async (ctx) => {
   // check if document with same id exists
   const { document } = await documentRead("users", userID);
 
-  if (document) {
+  if (document && document.afkAt) {
     documentUpdate(userID, { afkAt: serverTimestamp(), reason });
     reason
       ? ctx.reply(
@@ -67,6 +76,61 @@ bot.command("afk", async (ctx) => {
         getReplyOptions(ctx.message)
       )
     : ctx.reply(`${nameTag} is now afk!`, getReplyOptions(ctx.message));
+});
+
+// remove if was afk and messaged
+bot.on("message", async (ctx) => {
+  const userID = `${getUserID(ctx)}`;
+  const nameTag = fullNameWithTag(ctx);
+
+  const { document } = await documentRead("users", userID);
+
+  // if the user was afk
+  if (document && document.afkAt) {
+    // get how much time the user was afk
+    const afkInterval = formatDistanceToNowStrict(document.afkAt.toDate(), {
+      roundingMethod: "round"
+    });
+
+    const resetData = {
+      afkAt: null,
+      reason: null
+    };
+    documentUpdate(userID, resetData);
+    ctx.reply(
+      `${nameTag} is no longer afk!\nThey were afk for ${afkInterval}.`,
+      getReplyOptions(ctx.message)
+    );
+  }
+
+  // if someone else tags afk person
+  if (ctx.message.reply_to_message) {
+    const { taggedUserID, taggedUserNameTag } = getTaggedPersonData(ctx);
+
+    // fetch tagged person's data
+    const { document: taggedUserDocument } = await documentRead(
+      "users",
+      taggedUserID
+    );
+
+    // see if the tagged person was afk
+    if (taggedUserDocument && taggedUserDocument.afkAt) {
+      const afkInterval = formatDistanceToNowStrict(
+        taggedUserDocument.afkAt.toDate(),
+        { roundingMethod: "round", addSuffix: true }
+      );
+
+      taggedUserDocument.reason
+        ? ctx.reply(
+            `${taggedUserNameTag} is afk.\n Last seen ${afkInterval}.\n<strong>Reason:</strong> ${taggedUserDocument.reason}`,
+            getReplyOptions(ctx.message)
+          )
+        : ctx.reply(
+            `${taggedUserNameTag} is afk.\n Last seen ${afkInterval}.`,
+            getReplyOptions(ctx.message)
+          );
+    }
+  }
 });
 
 // deployment
